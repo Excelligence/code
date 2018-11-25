24fc351b85df6d641767d9dd77d82b3cd408be90
excelligence <excelligence.systems@gmail.com>
2018-11-25 18:34:35 +0530

    m

--- scripts/nodeScripts/jokeApiMob/details.js
+++
@@ -1,4 +0,0 @@
-
-const mlabDet='mongodb://excel:3xcell@ds161446.mlab.com:61446/quotesapi'
-
-module.exports.mlabDet=mlabDet;
---
+++ snippets/objFunc.js
@@ -0,0 +1,5 @@
+//How to define a function with named parameters 
+function func ({par1,par2}) {
+	console.log(par1,par2)
+}
+func({par2:'doe',par1:'john'})
