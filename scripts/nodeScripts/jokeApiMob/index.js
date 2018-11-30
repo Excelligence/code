49524f4fe5820dcd61dc086abe368cd6b59889cb
excelligence <excelligence.systems@gmail.com>
2018-11-30 14:04:48 +0530

    mess

--- scripts/nodeScripts/jokeApiMob/index.js
+++ scripts/nodeScripts/jokeApiMob/index.js
@@ -6,6 +6,12 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const details= require('./details')
 const Quotes= require('./db')
+const exphbs  = require('express-handlebars');
+ 
+ 
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+ 
 mongoose.connect(details.mlabDet);
  
 // parse application/x-www-form-urlencoded
@@ -22,6 +28,10 @@ app.use(bodyParser.json())
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname,'index.html'))
 })
+app.get('/home',function(req,res){
+	res.render('home',{quote:'this is a quote',
+	author:'gandhi'})
+})
 app.post('/addQuote',function(req,res){
 Quotes.create(req.body, function (err) {
   if (err) console.log(err);
@@ -34,9 +44,9 @@ app.get('/searchAuthor', function (req, res) {
 app.post('/searchAuthor',function (req,res){
 
 	Quotes.find({author : req.body.searchQuery},function(err,docs){
-    docs.forEach(function(doc){
-    console.log(doc.quote)
-  })
+res.render('home',{docs:docs})
+if(err)		
+console.log(err)		
 	})
 })
 app.listen(3000)
--- scripts/pythonScripts/quotesscrap.py
+++ scripts/pythonScripts/quotesscrap.py
@@ -1,12 +1,35 @@
 import requests
 from sendMail import sendMail
 from bs4 import BeautifulSoup
+from pymongo import MongoClient
+client = MongoClient('mongodb://excel:3xcell@ds161446.mlab.com:61446/quotesapi')
+db=client.quotesapi
+quotes=db.quotes
+
+post_data = {
+    'author': 'Mahatma Gandhi',
+    'quote': 'Be yourself'
+}
+#result = quotes.insert_one(post_data)
+#print('One: {0}'.format(result.inserted_id))
+
 author=input('Enter author name')
 site=requests.get(f'https://www.brainyquote.com/authors/{author}')
 soup=BeautifulSoup(site.content,'html.parser')
+
 images=soup.find_all('img')
-quotes=""
+#print(images)
+quotess=[]
 for image in images:
-   quotes+= (image.get('alt'))
-sendMail('excelligence@mail.com','DGj4bA2_yC4ig8u',quotes)
-print(quotes)
+    dict={}
+    dict['author']='Mahatma Gandhi'
+    dict['quote']=image.get('alt')
+    #print(dict)
+    quotes.insert_one(dict)
+    #dict="{'author':'Mahatma Gandhi','quote':"+(image.get('alt'))+"}"
+    #quotess.append(dict)
+ #  quotes+= (image.get('alt'))
+#sendMail('excelligence@mail.com','DGj4bA2_yC4ig8u',quotes)
+#for quote in quotess:
+ #   quotes.insert_one(quote)
+print('succes')    
